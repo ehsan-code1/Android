21f0ba315133029ddd03f6f17c4c8b778ca190da
mellero <m_ellero@hotmail.com>
2021-03-10 15:24:42 -0500

    Initial setup of trivia subapplication. Adds the trivia landing activity, a header xml, and intent.

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.example.myapplication.trivia.TriviaActivityLanding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.trivia_start).setOnClickListener {
+            val goToTriviaIntent = Intent(this, TriviaActivityLanding::class.java)
+            startActivity(goToTriviaIntent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/myapplication/trivia/TriviaActivityLanding.kt
@@ -0,0 +1,14 @@
+package com.example.myapplication.trivia
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.R
+
+class TriviaActivityLanding : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_trivia_landing)
+
+    }
+}
