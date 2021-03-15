02d4741ddd2b96d3055f0da3528e4936fcb3dad5
mellero <m_ellero@hotmail.com>
2021-03-14 21:37:39 -0400

    Adds click functionality to the difficulty buttons on the landing page for the Trivia activity. Some code cleanup and refactoring of onCreate and LandingClickListener, and strings.xml - including extracting hardcoded values, and refactoring names to be in line with "t_" prefix.

--- app/src/main/java/com/example/myapplication/trivia/TriviaActivityLanding.kt
+++ app/src/main/java/com/example/myapplication/trivia/TriviaActivityLanding.kt
@@ -2,6 +2,7 @@ package com.example.myapplication.trivia
 
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageButton
 import android.widget.Toast
 import android.widget.ToggleButton
 import androidx.appcompat.app.AppCompatActivity
@@ -18,16 +19,14 @@ class TriviaActivityLanding : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_trivia_landing)
 
-        setQTypeListeners()
-
-    }
-
-    /**
-     * Sets the click listeners for the Question Type (Multiple Choice and/or True/False)
-     */
-    private fun setQTypeListeners() {
-        findViewById<ToggleButton>(R.id.q_type_mc).setOnClickListener(LandingClickListener())
-        findViewById<ToggleButton>(R.id.q_type_tf).setOnClickListener(LandingClickListener())
+        /* Set Question Type Listeners */
+        findViewById<ToggleButton>(R.id.t_q_type_mc).setOnClickListener(LandingClickListener())
+        findViewById<ToggleButton>(R.id.t_q_type_tf).setOnClickListener(LandingClickListener())
+        /* Set Question Difficulty Listeners */
+        arrayOf(R.id.t_diff_easy_btn, R.id.t_diff_med_btn, R.id.t_diff_hard_btn)
+                .forEach {
+                    findViewById<ImageButton>(it).setOnClickListener(LandingClickListener())
+                }
     }
 
     /**
@@ -41,26 +40,73 @@ class TriviaActivityLanding : AppCompatActivity() {
         override fun onClick(v: View) {
             when (v.id) {
                 /* Question Type Listeners */
-                R.id.q_type_tf, R.id.q_type_mc -> {
-                    /* Sets the toast message for the Question Type buttons */
-                    val toastMessage = setQTypeMessage()
-
-                    // Check if a toast already exists, if it does, cancel it
-                    // @ symbol is a "qualified this", used to denote specific scopes being used
-                    if (this@TriviaActivityLanding::currentToast.isInitialized) {
-                        currentToast.cancel()
-                        if (toastMessage == "") {
-                            return
-                        }
-                    }
-                    currentToast = Toast.makeText(
-                            this@TriviaActivityLanding,
-                            getString(R.string.t_qtype_toast, toastMessage),
-                            Toast.LENGTH_SHORT
-                    )
-                    currentToast.show()
+                R.id.t_q_type_tf, R.id.t_q_type_mc -> {
+                    setQTypeListeners()
+                }
+                /* Difficulty Mode Listeners */
+                R.id.t_diff_easy_btn, R.id.t_diff_med_btn, R.id.t_diff_hard_btn -> {
+                    setQDifficultyListeners(v.id)
                 }
             }
+        }
+
+        /**
+         * Sets the click listeners for the Question Type (Multiple Choice and/or True/False)
+         */
+        private fun setQTypeListeners() {
+            /* Sets the toast message for the Question Type buttons */
+            val toastMessage = setQTypeMessage()
+
+            // Check if a toast already exists, if it does, cancel it
+            // @ symbol is a "qualified this", used to denote specific scopes being used
+            if (this@TriviaActivityLanding::currentToast.isInitialized) {
+                currentToast.cancel()
+                if (toastMessage == "") {
+                    return
+                }
+            }
+            currentToast = Toast.makeText(
+                    this@TriviaActivityLanding,
+                    getString(R.string.t_qtype_toast, toastMessage),
+                    Toast.LENGTH_SHORT
+            )
+            currentToast.show()
+        }
+
+        /**
+         * Sets the click listeners for the Question Difficulty Buttons
+         */
+        private fun setQDifficultyListeners(id: Int) {
+            // Set all Difficulty mode buttons to half-alpha
+            arrayOf(R.id.t_diff_easy_btn, R.id.t_diff_med_btn, R.id.t_diff_hard_btn)
+                    .forEach {
+                        findViewById<ImageButton>(it).alpha = 0.5F
+                    }
+            // Set clicked button alpha to original level, so as to highlight the choice
+            findViewById<ImageButton>(id).alpha = 1.0F
+
+            val toastMessage =
+                    when (id) {
+                        R.id.t_diff_easy_btn -> "Easy Mode"
+                        R.id.t_diff_med_btn -> "Medium Mode"
+                        R.id.t_diff_hard_btn -> "Hard Mode"
+                        else -> ""
+                    }
+
+            // Check if a toast already exists, if it does, cancel it
+            if (this@TriviaActivityLanding::currentToast.isInitialized) {
+                currentToast.cancel()
+                if (toastMessage == "") {
+                    return
+                }
+            }
+
+            currentToast = Toast.makeText(
+                    this@TriviaActivityLanding,
+                    toastMessage,
+                    Toast.LENGTH_SHORT
+            )
+            currentToast.show()
         }
 
         /**
@@ -68,12 +114,12 @@ class TriviaActivityLanding : AppCompatActivity() {
          * @return a string stating which buttons are selected, or an empty string if none are
          */
         private fun setQTypeMessage(): String {
-            return if (findViewById<ToggleButton>(R.id.q_type_mc).isChecked
-                    && findViewById<ToggleButton>(R.id.q_type_tf).isChecked)
+            return if (findViewById<ToggleButton>(R.id.t_q_type_mc).isChecked
+                    && findViewById<ToggleButton>(R.id.t_q_type_tf).isChecked)
                 "Multiple Choice & True/False"
-            else if (findViewById<ToggleButton>(R.id.q_type_mc).isChecked)
+            else if (findViewById<ToggleButton>(R.id.t_q_type_mc).isChecked)
                 "Multiple Choice"
-            else if (findViewById<ToggleButton>(R.id.q_type_tf).isChecked)
+            else if (findViewById<ToggleButton>(R.id.t_q_type_tf).isChecked)
                 "True/False"
             else
                 ""
