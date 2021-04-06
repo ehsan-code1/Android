1e730787444d7802a4e75bdd44952069f0269b94
mellero <m_ellero@hotmail.com>
2021-04-05 21:08:40 -0400

    Completes Nav Drawer implementation (for MainActivity). Allows for navigation to each section of the project through the nav drawer, or through the main buttons on the page.

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.example.myapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Button
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.example.myapplication.car_database.HomePage
+import com.example.myapplication.songstar.SongsterSearch
+import com.example.myapplication.trivia.TriviaActivityLanding
+import com.google.android.material.internal.NavigationMenuItemView
+import com.google.android.material.navigation.NavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val tb : Toolbar? = findViewById(R.id.toolbar)
+        setSupportActionBar(tb)
+
+        val drawer : DrawerLayout = findViewById(R.id.drawer_layout)
+        val toggle  = ActionBarDrawerToggle(this, drawer, tb, R.string.open, R.string.close)
+
+        drawer.addDrawerListener(toggle)
+        toggle.syncState()
+
+        findViewById<NavigationView>(R.id.nav_view).setNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.nav_item_one -> goToTrivia()
+                R.id.nav_item_two -> goToSongster()
+                R.id.nav_item_three -> goToCarDatabase()
+            }
+
+            findViewById<DrawerLayout>(R.id.drawer_layout).closeDrawer(GravityCompat.START)
+            return@setNavigationItemSelectedListener true
+        }
+
+        findViewById<Button>(R.id.car).setOnClickListener{ goToCarDatabase() }
+        findViewById<Button>(R.id.trivia).setOnClickListener{ goToTrivia() }
+        findViewById<Button>(R.id.sstar).setOnClickListener{ goToSongster() }
+    }
+
+    private fun goToTrivia() {
+        val goToTrivia = Intent(this, TriviaActivityLanding::class.java)
+        startActivity(goToTrivia)
+    }
+
+    private fun goToSongster() {
+        val goToSongster = Intent(this, SongsterSearch::class.java)
+        startActivity(goToSongster)
+    }
+
+    private fun goToCarDatabase() {
+        val goToCarDatabase = Intent(this, HomePage::class.java)
+        startActivity(goToCarDatabase)
+    }
+}
